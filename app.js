5ac257451b88ef10ecf2cbce4067eeebd013a07a
Anikmajumder <53091562+Anikmajumder@users.noreply.github.com>
2022-05-10 21:18:05 +0600

    Added hashing

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
 //const _ = require("lodash"); 
-const encrytpt = require("mongoose-encryption");
+const md5= require("md5");
 
 const app = express();
 
-console.log(process.env.API_KEY);
+console.log(md5("123456"));
 
 app.set('view engine', 'ejs');
 
@@ -24,7 +24,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrytpt,{secret: secret, encryptedFields:["password"] });
 
 const User = new mongoose.model("User", userSchema );
 
@@ -44,7 +43,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -58,7 +57,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
